a42e3f7b683bc6940e7c8f86f4ca5223205b464e
Vitor Duarte <vitorduarte.vds7@gmail.com>
2022-04-25 21:32:02 -0300

    accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(({ title, content }) => {
+    // Obs: array destructure, the activeIndex is reaching the first[0] element of the array and assigning it to a new variable called "activeIndex" and the setActiveIndex is reaching the second[1] element of the array and assigning with the variable setActiveIndex
+    // the "" on useState("") is the initial value for the state, activeIndex="",then,setActiveIndex will fulfill as the changes are made
+    const [activeIndex, setActiveIndex] = useState("");
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+    const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? "active" : "";
         return (
-            <React.Fragment key={title}>
-                <div className="title active">
+            <React.Fragment key={item.title}>
+                <div
+                    className={`title${active}`}
+                    onClick={() => {
+                        onTitleClick(index);
+                    }}
+                >
                     <i className="dropdown icon"></i>
-                    {title}
+                    {item.title}
                 </div>
-                <div className="content active">
-                    <p>{content}</p>
+                <div className={`content ${active}`}>
+                    <p>{item.content}</p>
                 </div>
             </React.Fragment>
         );

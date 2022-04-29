f48f9f89b11dc9814a37face72a18a9a39a66db3
Vitor Duarte <vitorduarte.vds7@gmail.com>
2022-04-29 12:47:58 -0300

    Translation done

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
     {
@@ -13,6 +14,14 @@ const options = [
     {
         label: "Hindi",
         value: "hi",
+    },
+    {
+        label: "Portuguese",
+        value: "pt",
+    },
+    {
+        label: "English",
+        value: "en",
     },
 ];
 
@@ -40,6 +49,9 @@ const Translate = () => {
                 onSelectedChange={setLanguage}
                 options={options}
             />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 };
